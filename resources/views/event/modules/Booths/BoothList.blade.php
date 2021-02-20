36f33bbc0aefcc413e272397555b73857e5e460e
Ranjeet kumar  singh <mrphpguru@gmail.com>
2021-02-20 12:35:46 +0000

    BoothList.blade.php edited online with Bitbucket

--- resources/views/event/modules/Booths/BoothList.blade.php
+++ resources/views/event/modules/Booths/BoothList.blade.php
@@ -51,17 +51,18 @@
         </div>
        <div title="Bowman and Brooke LLP" class= "area candidate-booth positioned" style="width: 7%; height: 14%; top: 47%; left: 69%;" data-link="booth/d07859ae-ee57-4336-b808-cfc5a54457ff">
         </div>
-       <div title="Bradley Arant Boult Cummings LLP" class= "area candidate-booth positioned" style="width: 7%; height: 47%; top: 57%; left: 46%;" data-link="booth/a62bd00e-b182-481b-a298-b49a9c6c7de4">
+       <div title="Bradley Arant Boult Cummings LLP" class= "area candidate-booth positioned" style="width: 14%; height: 47%; top: 57%; left: 46%;" data-link="booth/a62bd00e-b182-481b-a298-b49a9c6c7de4">
         </div>
        <div title="GreenbergTraurig" class= "area candidate-booth positioned" style="width: 7%; height: 14%; top: 47%; left: 46%;" data-link="booth/45f94fcf-c355-4f36-8f0b-b91b7eb73d40">
         </div>
-       <div title="Hamilton, Milller & Birthisel" class= "area candidate-booth positioned" style="width: 7%; height: 47%; top: 34%; left: 21%;" data-link="booth/571f902e-1c21-4559-96eb-52045a0993f3">
+       <div title="Hamilton, Milller & Birthisel" class= "area candidate-booth positioned" style="width: 7%; height: 14%; top: 47%; left: 34%;" data-link="booth/571f902e-1c21-4559-96eb-52045a0993f3">
         </div>
 
-      <div title="Ivie McNeill Wyatt Purcell & Diggs" class= "area candidate-booth positioned" style="width: 7%; height: 47%; top: 23%; left: 9%;" data-link="booth/cc7508b6-8af5-4d3b-b3a9-81e61314aeba">
+       <div title="Ivie McNeill Wyatt Purcell & Diggs" class= "area candidate-booth positioned" style="width: 7%; height: 14%; top: 47%; left:23%;" data-link="booth/cc7508b6-8af5-4d3b-b3a9-81e61314aeba">
         </div>
 
-       <div title="Kuchler Polk Weiner, LLC" class= "area candidate-booth positioned" style="width: 7%;height: 12%;top: 47%;left: 12%;" data-link="booth/2edd03bf-11c9-46e5-b860-81d4d5370a0b">
+
+        <div title="Kuchler Polk Weiner, LLC" class= "area candidate-booth positioned" style="width: 7%;height: 12%;top: 47%;left: 12%;" data-link="booth/2edd03bf-11c9-46e5-b860-81d4d5370a0b">
         </div>
 
 
