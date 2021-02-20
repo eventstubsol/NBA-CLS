f1d183ba52118ddad84802fb5a0339e85040a27e
Ranjeet kumar  singh <mrphpguru@gmail.com>
2021-02-20 12:21:04 +0000

    BoothList.blade.php edited online with Bitbucket

--- resources/views/event/modules/Booths/BoothList.blade.php
+++ resources/views/event/modules/Booths/BoothList.blade.php
@@ -21,25 +21,32 @@
     <div class="booths position-relative" style="height: 100vh">
         <img src="{{ assetUrl(getField("walker_hall_image")) }}" class="positioned booth-bg" alt="">
        
-       <div title="Alston & Bird" class= "area candidate-booth positioned" style="width: 9%;   height: 16%;   top: 69%;  left: 85%" data-link="booth/e8432607-6666-456b-8a9f-f10237ba9d55">
+      
+       <div title="Alston & Bird" class= "area candidate-booth positioned" style="width: 9%;   height: 16%;   top: 63%;  left: 82%" data-link="booth/e8432607-6666-456b-8a9f-f10237ba9d55">
         </div>
-       <div title="Barnes & Thornburg LLP" class= "area candidate-booth positioned" style="width: 9%;   height: 16%;   top: 69%;  left: 75%" data-link="booth/42ea7e91-c0d1-4d51-a180-795b3ffe1e70">
+       <div title="Barnes & Thornburg LLP" class= "area candidate-booth positioned" style="width: 9%;   height: 16%;   top: 63%;  left: 72%" data-link="booth/42ea7e91-c0d1-4d51-a180-795b3ffe1e70">
         </div>
-       <div title="Carlton Fields" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 69%;  left: 63%" data-link="booth/e0a68b2e-db62-4d43-8162-60a535440573">
+       <div title="Carlton Fields" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 63%;  left: 61%" data-link="booth/e0a68b2e-db62-4d43-8162-60a535440573">
         </div>
-       <div title="Crowell & Moring" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 69%;  left: 51%" data-link="booth/fafc2096-a16f-4fb2-9fad-98b64e3426e4">
+       <div title="Crowell & Moring" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 63%;  left: 51%" data-link="booth/fafc2096-a16f-4fb2-9fad-98b64e3426e4">
         </div>
-       <div title="DLA Piper" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 69%;  left: 38%" data-link="booth/0222d945-75dd-455a-b0ba-f22185a19cd9">
+       <div title="DLA Piper" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 63%;  left: 38%" data-link="booth/0222d945-75dd-455a-b0ba-f22185a19cd9">
         </div>
-       <div title="Fasken" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 69%;  left: 26%" data-link="booth/ef36787e-36f1-4ebc-8ce8-d0e92499f0e7">
+       <div title="Fasken" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 63%;  left: 40%" data-link="booth/ef36787e-36f1-4ebc-8ce8-d0e92499f0e7">
         </div>
-       <div title="Hogan Lovells" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 69%;  left: 14%" data-link="booth/e830614d-f55f-4480-91e1-d56f0b3b04a8">
+       <div title="Hogan Lovells" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 63%;  left: 29%" data-link="booth/e830614d-f55f-4480-91e1-d56f0b3b04a8">
         </div>
-       <div title="Fish & Richardson" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 69%;  left: 2%" data-link="booth/7e7e4d9e-f43c-4359-b9ee-7c7246067d97">
+       <div title="Fish & Richardson" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 62%;  left: 19%" data-link="booth/7e7e4d9e-f43c-4359-b9ee-7c7246067d97">
+        </div>
+
+        <div title="K&L Gates" class= "area candidate-booth positioned" style="width: 9%;  height: 16%;   top: 63%;  left: 8%" data-link="booth/0f6d6ba7-ba9c-47d4-a012-83ed8e55efe2">
         </div>
         
-       <div title="K&L Gates" class= "area candidate-booth positioned" style="width: 8%;height: 13%; top: 55%; left: 83%;" data-link="booth/0f6d6ba7-ba9c-47d4-a012-83ed8e55efe2">
-        </div>
+       
+
+
+
+     
        <div title="McGuireWoods" class= "area candidate-booth positioned" style=" width: 8%;height: 13%;top: 55%;left: 71%;" data-link="booth/20ba9089-3e22-481e-80b9-9218f5361751">
         </div>
        <div title="Bowman and Brooke LLP" class= "area candidate-booth positioned" style="width: 7%; height: 14%; top: 55%; left: 59%;" data-link="booth/d07859ae-ee57-4336-b808-cfc5a54457ff">
