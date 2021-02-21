0be8c0ff5302e5a64f898b3eacd8f46f7e8b7452
FITS MEA <event-admin@eventstub.co>
2021-02-21 10:55:57 +0000

    SingleBooth.blade.php edited online with Bitbucket

--- resources/views/event/modules/Booths/SingleBooth.blade.php
+++ resources/views/event/modules/Booths/SingleBooth.blade.php
@@ -198,7 +198,7 @@
     <div class="modal-dialog  modal-lg modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="myLargeModalLabel">Firm Atendees</h4>
+                <h4 class="modal-title" id="myLargeModalLabel">Firm Attendees</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
             </div>
             <div class="modal-body">
