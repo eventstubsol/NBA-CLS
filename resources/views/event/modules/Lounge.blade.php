1ecfd6c37a750cca10d5281c9ea2352afd590471
shubh-palan <63784377+shubh-palan@users.noreply.github.com>
2021-02-23 00:22:27 +0530

    Museum removed

--- resources/views/event/modules/Lounge.blade.php
+++ resources/views/event/modules/Lounge.blade.php
@@ -2,11 +2,21 @@
     <div style="position: relative; height:100vh">
         <img src="{{ assetUrl(getField("networkingLounge")) }}" class="positioned booth-bg" alt="" />
         @foreach(LOUNGE_AREAS as $area)
+            @if(isset($area['zoom_url']))
+            <a title="{{ $area['title']}}" 
+                class="positioned"
+                style="{{ areaStyles($area['area']) }}"
+                href="{{ $area['zoom_url'] }}"
+                target="_blank"
+            ></a>
+
+            @else
             <div
                     title="{{ $area['title'] }}"
                     class="positioned area"
                     data-link="{{ $area['link'] }}"
                     style="{{ areaStyles($area["area"]) }}"></div>
+            @endif
         @endforeach
         {!! getScavengerItems("lounge") !!}
     </div>
